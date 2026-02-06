07bbdf7ffa12ffc21ca09e3ba2c7c3c373c7c331
prakhargautam425 <prakhargautam425@gmail.com>
2026-02-06 23:20:42 +0530

    added fragments

--- app/src/main/java/com/example/notificationhistory/MainActivity.java
+++ app/src/main/java/com/example/notificationhistory/MainActivity.java
@@ -7,11 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +25,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Default fragment
+        loadFragment(new NotificationsFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            Fragment fragment = null;
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_notifications) {
+                fragment = new NotificationsFragment();
+
+            } else if (id == R.id.nav_filters) {
+                fragment = new FiltersFragment();
+
+            } else if (id == R.id.nav_settings) {
+                fragment = new SettingsFragment();
+            }
+
+            return loadFragment(fragment);
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
